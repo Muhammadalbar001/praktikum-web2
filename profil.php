a8b5c8685549217bdbf0b50b5c59a82b8056ba2a
Muhammadalbar001 <378muhammadalbar@gmail.com>
2024-06-05 13:30:52 +0800

    Initial commit

---
+++ profil.php
@@ -0,0 +1,54 @@
+<div class="row">
+    <div class="col-md-12">
+        <div class="text-center">
+            <img src="img/albar.jpg" alt="Muhammad Albar" width="160" class="rounded-circle">
+            <table class="table mt-4" style="width:100%; margin:0 auto;">
+                <tbody>
+                    <tr>
+                        <td align="right" width="50%"><strong>NIK / NIDN :</strong></td>
+                        <td align="left" width="50%">2210010667 / 2210010667</td>
+                    </tr>
+                    <tr>
+                        <td align="right" width="50%"><strong>Fakultas / Program Studio :</strong></td>
+                        <td align="left" width="50%">Teknologi Informasi / Teknik Informatika</td>
+                    </tr>
+                    <tr>
+                        <td align="right" width="50%"><strong>Nama Lengkap :</strong></td>
+                        <td align="left" width="50%">Muhammad Albar</td>
+                    </tr>
+                    <tr>
+                        <td align="right" width="50%"><strong>Tempat, Tanggal Lahir :</strong></td>
+                        <td align="left" width="50%">Kampung Baru, 26 September 2002</td>
+                    </tr>
+                    <tr>
+                        <td align="right" width="50%"><strong>Telepon :</strong></td>
+                        <td align="left" width="50%">083141886688</td>
+                    </tr>
+                    <tr>
+                        <td align="right" width="50%"><strong>email :</strong></td>
+                        <td align="left" width="50%">378muhammadalbar@gmail.com</td>
+                    </tr>
+                    <tr>
+                        <td align="right" width="50%"><strong>Media Sosial :</strong></td>
+                        <td align="left" width="50%">
+                            <a href="https://facebook.com/muhammad.albar.507464" target="_blank"
+                                style="text-decoration:none;">
+                                <img src="img/medsos/facebook.svg" width="30" alt="facebook">
+                            </a>
+                            <a href="http://instagram.com/xmalbar_" target="_blank" style="text-decoration:none;">
+                                <img src="img/medsos/instagram.svg" width="30" alt="instagram">
+                            </a>
+                            <a href="http://twitter.com/exmalbar" target="_blank" style="text-decoration:none;">
+                                <img src="img/medsos/twitter.svg" width="30" alt="twitter">
+                            </a>
+                            <a href="http://linkedin.com/in/" target="_blank">
+                                <img src="img/medsos/linkedin.svg" width="30" alt="linkedin">
+                            </a>
+                        </td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
+    </div>
+</div>
+<?php include "footer.php" ?>
